33cadc50282874048e02fd1361de1f084d29d88c
Abhishek Vyas <abhishekvvyas@gmail.com>
2025-03-01 17:44:40 -0330

    updated for vercel

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,10 @@ const start = async () => {
 		await prisma.$connect();
 		logger.info("Database connection established successfully");
 
+		app.get("/", async (req, res) => {
+			return res.status(200).type("text/html").send("html");
+		});
+
 		// Start server
 		await app.listen({ port: config.app.port as number, host: "0.0.0.0" });
 		logger.info(`Server started on port ${config.app.port}`);
